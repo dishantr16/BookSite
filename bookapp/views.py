15e14c6a893063040260f95926a606834b782da2
Dishant Rathi <dishant16rathi@gmail.com>
2021-03-08 11:50:06 +0530

    templates created base.html inserted

--- bookapp/views.py
+++ bookapp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Book, Category
 
-# Create your views here.
+
 
 def home(request):
-    return render(request, 'home.html', {})
+    recommended_books = Book.objects.filter(recommended_books = True)
+    fiction_books = Book.objects.filter(fiction_books = True)
+    business_books = Book.objects.filter(business_books = True)
+    return render(request, 'home.html', {
+        'recommended_books': recommended_books,
+        'fiction_books': fiction_books,
+        'business_books': business_books,
+    })
