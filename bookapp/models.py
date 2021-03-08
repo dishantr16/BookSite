21375eb31de76134310a1331d9bddb888bd026f2
Dishant Rathi <dishant16rathi@gmail.com>
2021-03-08 11:09:25 +0530

    models created of class book and category

--- bookapp/models.py
+++ bookapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+
+    name = models.CharField('categories',max_length=200)
+    slug = models.SlugField(max_length=200)
+        
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    cover_image = models.ImageField(upload_to='img', blank=True, null=True)
+    author = models.CharField(max_lenth=100)
+    summary = models.TextField()
+    category = models.ManyToManyField(Category, related_name='books')
+    pdf = models.FileField(upload_to='pdf')
+    recommended_books = models.BooleanField(default=False)
+    fiction_books = models.BooleanField(default=False)
+    business_books = models.BooleanField(default=False)
