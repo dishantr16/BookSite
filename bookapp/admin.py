8eca5d770509ff3dfd8df0eebd9496d367b2d1fe
Dishant Rathi <dishant16rathi@gmail.com>
2021-03-08 11:33:59 +0530

    created templated added category in the db

--- bookapp/admin.py
+++ bookapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Category, Book
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Book, BookAdmin)
--- bookstore/urls.py
+++ bookstore/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('bookapp/', include('bookapp.urls')),
+    path('', include('bookapp.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
